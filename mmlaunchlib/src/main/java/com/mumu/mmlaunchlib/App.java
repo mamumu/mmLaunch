91309ba44a94141c8668c79d2f8d4fe4a71bd978
DESKTOP-I41S58S\zhulinfeng <zhulinfeng@vcredit.com>
2018-06-07 17:23:19 +0800

    xiug

---
+++ mmlaunchlib/src/main/java/com/mumu/mmlaunchlib/App.java
@@ -0,0 +1,14 @@
+package com.mumu.mmlaunchlib;
+
+import android.app.Application;
+
+/**
+ * 作者　　: 朱林峰
+ * 创建时间: 2018/5/25
+ * 邮箱　　：mamumuma1001@sina.com
+ * <p>
+ * 功能介绍：
+ */
+public class App extends Application{
+
+}
---
+++ mmlaunchlib/src/main/java/com/mumu/mmlaunchlib/GuideActivity.java
@@ -0,0 +1,12 @@
+package com.mumu.mmlaunchlib;
+
+/**
+ * 作者　　: 朱林峰
+ * 创建时间: 2018/5/25
+ * 邮箱　　：mamumuma1001@sina.com
+ * <p>
+ * 功能介绍：
+ */
+public class GuideActivity {
+
+}
